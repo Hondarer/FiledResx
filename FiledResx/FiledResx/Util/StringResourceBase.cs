926e9de288393d6c8dc50df64e6cad82bdd24959
Tetsuo Honda <t-honda@hondarer-soft.com>
2021-02-04 15:57:37 +0900

    リファクタリング

--- FiledResx/FiledResx/Util/StringResourceBase.cs
+++ FiledResx/FiledResx/Util/StringResourceBase.cs
@@ -92,10 +92,7 @@ namespace FiledResx.Resources
         /// </param>
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         #endregion
